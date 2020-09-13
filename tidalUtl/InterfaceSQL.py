4862b2124c734f93f8f1fa7dbb54cba9cfb4bbf8
Shin Anase <F8hs02a1kX@gmail.com>
2020-09-13 11:41:51 +0900

    Update作成途中

--- tidalUtl/InterfaceSQL.py
+++ tidalUtl/InterfaceSQL.py
@@ -1,6 +1,9 @@
 import psycopg2
 import pandas as pd
 
+#ExecDataの作成
+#Input：DB名
+#Output：なし
 def createExecData(dbname):
     with psycopg2.connect("host=localhost port=5432 dbname=" + dbname + " user=tidal password=tidalryoku") as conn:
         with conn.cursor() as cur:
@@ -9,7 +12,10 @@ def createExecData(dbname):
     
     print("DONE.")
 
-
+    
+#列名の配列抽出
+#Input：DB名、テーブル名
+#Output：列名(dataframe)
 def readColumns(dbname, tablename):
     with psycopg2.connect("host=localhost port=5432 dbname=" + dbname + " user=tidal password=tidalryoku") as conn:
         with conn.cursor() as cur:
@@ -20,6 +26,10 @@ def readColumns(dbname, tablename):
     
     return df_columns
 
+
+#ExecDataの読み込み
+#Input：DB名、テーブル名、抽出するcolumn名(指定しなければ前列を抽出する)
+#Output：指定した列のテーブル(dataframe)
 def selectExecData(dbname, tableName, clmns = None):
     with psycopg2.connect("host=localhost port=5432 dbname=" + dbname + " user=tidal password=tidalryoku") as conn:
         with conn.cursor() as cur:
@@ -46,4 +56,45 @@ def selectExecData(dbname, tableName, clmns = None):
                 df.columns = colnames
 
     return df
+
+
+#指定列のUpdate(列ごと挿げ替え)
+#Input：DB名、テーブル名、挿げ替えるtable(dataframe)
+#Output：なし
+def exchangeExecData(dbname, tableName, df):
+    #列名取得
+    clmnNns = df.columns
+    
+    #挿げ替える列名削除用query作成
+    qrySnpt = ""
+    for nm in clmnNns:
+        qrySnpt = qrySnpt + "DROP COLUMN " + nm + ", "
+    qrySnpt = qrySnpt[:-2]
+    qrySnpt += ";"
+    Query = "ALTER TABLE " + tableName +  " " + qrySnpt
+    
+    #挿げ替える前の列削除
+    with psycopg2.connect("host=localhost port=5432 dbname=" + dbname + " user=tidal password=tidalryoku") as conn:
+        with conn.cursor() as cur:
+            cur.execute(Query)
+    
+    #列挿げ替え用のquery作成
+
+
+
+#指定列のUpdate
+#Input：DB名、テーブル名、更新するtable(dataframe)、主キーとなるtable(dataframe)
+#Output：なし
+def updateExecData(dbname, tableName, updDf, pkeyDf):
+    #列名取得
+    updClmnNns = updDf.columns
+    pkeyClmnNns = pkeyDf.columns
+    
+    #指定列のUpdate実行
+    with psycopg2.connect("host=localhost port=5432 dbname=" + dbname + " user=tidal password=tidalryoku") as conn:
+        with conn.cursor() as cur:
+            
+            
+
+            
                 
